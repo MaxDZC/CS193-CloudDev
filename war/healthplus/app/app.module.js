f69fbc4463982fce1b811b922a3ca256ca05cb25
Patricia Awayan <pawayan1@yahoo.com>
2017-07-03 23:57:11 +0800

    integrated registermedicine

--- war/healthplus/app/app.module.js
+++ war/healthplus/app/app.module.js
@@ -4,18 +4,21 @@ require('./components/explorediseases/explorediseases.main.js');
 require('./shared/footer/footer.main.js');
 require('./components/registerdoctor/registerdoctor.main.js');
 require('./components/registerdiseases/registerdiseases.main.js');
+require('./components/registermedicine/registermedicine.main.js');
 require('./components/viewdoctor/viewdoctor.main.js');
 require('./components/editdoctor/editdoctor.main.js');
 require('./components/exploredoctors/exploredoctors.main.js');
 require('./components/exploremedicines/exploremedicines.main.js');
 require('./components/userLogin/loginForm.main.js');
 require('./shared/parts/pagination/pagination.main.js');
+
 angular.module('hplus.modules', [
   'hplus.modules.header',
   'hplus.modules.navbar',
   'hplus.modules.explorediseases',
   'hplus.modules.registerdoctor',
   'hplus.modules.registerdiseases',
+  'hplus.modules.registermedicine',
   'hplus.modules.footer',
   'hplus.modules.viewdoctor',
   'hplus.modules.editdoctor',
