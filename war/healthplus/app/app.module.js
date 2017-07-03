4e99ec582e1b51b02131ea5d25287a6bd40f8987
horanithegreat <f.fly60@yahoo.com>
2017-07-03 21:50:21 +0800

    adding exploredoctors & medicines features --by Yanal Horani

--- war/healthplus/app/app.module.js
+++ war/healthplus/app/app.module.js
@@ -4,6 +4,8 @@ require('./components/explorediseases/explorediseases.main.js');
 require('./shared/footer/footer.main.js');
 require('./components/registerdoctor/registerdoctor.main.js');
 require('./components/viewdoctor/viewdoctor.main.js');
+require('./components/exploredoctors/exploredoctors.main.js');
+require('./components/exploremedicines/exploremedicines.main.js');
 
 angular.module('hplus.modules', [
   'hplus.modules.header',
@@ -11,6 +13,8 @@ angular.module('hplus.modules', [
   'hplus.modules.explorediseases',
   'hplus.modules.footer',
   'hplus.modules.registerdoctor',
-  'hplus.modules.viewdoctor'
+  'hplus.modules.viewdoctor',
+  'hplus.modules.exploredoctors',
+  'hplus.modules.exploremedicines'
 ]);
 
