0b9238a9a824c68f07dd7f201e494b1c936a065b
Mac User <macuser@Macs-MacBook-Pro.local>
2017-07-03 22:03:13 +0800

    Login Form Added route: user/login

--- war/healthplus/app/app.module.js
+++ war/healthplus/app/app.module.js
@@ -4,13 +4,14 @@ require('./components/explorediseases/explorediseases.main.js');
 require('./shared/footer/footer.main.js');
 require('./components/registerdoctor/registerdoctor.main.js');
 require('./components/viewdoctor/viewdoctor.main.js');
-
+require('./components/userLogin/loginForm.main.js');
 angular.module('hplus.modules', [
   'hplus.modules.header',
   'hplus.modules.navbar',
   'hplus.modules.explorediseases',
   'hplus.modules.footer',
   'hplus.modules.registerdoctor',
-  'hplus.modules.viewdoctor'
+  'hplus.modules.viewdoctor',
+  'hplus.modules.userLogin'
 ]);
 
