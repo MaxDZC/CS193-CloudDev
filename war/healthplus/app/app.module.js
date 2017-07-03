6ef05ddb42788639217a86d7e5ebe2440652d549
redsaph <redsaph@ymail.com>
2017-07-03 20:44:32 +0800

    Added View Doctor page. Fixed broken navbar button height. Added icons on buttons. Made design for pagination.

--- war/healthplus/app/app.module.js
+++ war/healthplus/app/app.module.js
@@ -2,11 +2,13 @@ require('./shared/header/header.main.js');
 require('./shared/navbar/navbar.main.js');
 require('./components/explorediseases/explorediseases.main.js');
 require('./components/registerdoctor/registerdoctor.main.js');
+require('./components/viewdoctor/viewdoctor.main.js');
 
 angular.module('hplus.modules', [
   'hplus.modules.header',
   'hplus.modules.navbar',
   'hplus.modules.explorediseases',
-  'hplus.modules.registerdoctor'
+  'hplus.modules.registerdoctor',
+  'hplus.modules.viewdoctor',
 ]);
 
