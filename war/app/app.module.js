bbf814018c68c596eac32a961b4a4690270f37fe
redsaph <redsaph@ymail.com>
2017-07-21 15:44:09 +0800

    Create View Patient UI.

--- war/app/app.module.js
+++ war/app/app.module.js
@@ -22,6 +22,7 @@ require('./components/login/login.main.js');
 require('./components/viewdoctor/viewdoctor.main.js');
 require('./components/viewdisease/viewdisease.main.js');
 require('./components/viewmedicine/viewmedicine.main.js');
+require('./components/viewpatient/viewpatient.main.js');
 require('./components/resetpassword/resetpassword.main.js');
 
 angular.module('hplus.modules', [
@@ -44,6 +45,7 @@ angular.module('hplus.modules', [
   'hplus.modules.viewdisease',
   'hplus.modules.viewdoctor',
   'hplus.modules.viewmedicine',
+  'hplus.modules.viewpatient',
   'hplus.modules.resetpassword'
 ]);
 
