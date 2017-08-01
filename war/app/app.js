eaeaf3e6c920b31a8420ed521198c0fb64b87497
Max <maxtimedout@gmail.com>
2017-08-01 09:41:59 +0800

    ???

--- war/app/app.js
+++ war/app/app.js
@@ -20,6 +20,7 @@ app.config(function ($routeProvider, $locationProvider){
       redirectTo: '/'
     });  
 
+  	$locationProvider.html5Mode(true);
     // $locationProvider.html5Mode({
     //   enabled: true,
     //   requireBase: false
