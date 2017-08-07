70f8f1a37a5f690a97c4fcf67a1af76e083919cb
Patricia Awayan <pawayan1@yahoo.com>
2017-08-08 03:28:29 +0800

    added email input thing in registerdoctor; changes in registerdiseases

--- war/app/components/registerdiseases/registerdiseases.controller.js
+++ war/app/components/registerdiseases/registerdiseases.controller.js
@@ -6,5 +6,25 @@ angular.module('hplus.modules.registerdiseases')
       $scope.go = function(path){
         globalFactory.go(path);
       };
+      
+      $scope.symptoms = [
+        {
+          name: "Examples",
+          id: 1
+        },
+        {
+          name: "of",
+          id: 2
+        },
+        {
+          name: "Symptoms",
+          id: 3
+        },
+        {
+          name: "wooh",
+          id: 4
+        }
+      ];
+
     }
   );
