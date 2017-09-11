f9f092fe144f7c4ac072798116d6f525ede6498f
redsaph <redsaph@ymail.com>
2017-09-11 21:11:56 +0800

    Explore Doctors is now able to search for doctors from a dynamically populated list. Pagination also works. Also created a new directive for cards which can be used system-wide.

--- war/app/app.js
+++ war/app/app.js
@@ -2,19 +2,21 @@ require("../assets/main.scss");
 
 require('angular');
 require('angular-route');
-require('./app.module.js')
+require('./app.module.js');
+require('angular-utils-pagination');
 require('./shared/parts/modal/newFile.js');
 require('./shared/parts/modal/SweetAlert.min.js');
 
 var app = angular.module('hplus', [
                             'ngRoute',
                             'hplus.modules',
-                            'oitozero.ngSweetAlert'
+                            'oitozero.ngSweetAlert',
+                            'angularUtils.directives.dirPagination'
                           ]
                         );
 
 
-app.config(function ($routeProvider, $locationProvider){  
+app.config(function ($routeProvider, $locationProvider, paginationTemplateProvider){  
   $routeProvider   
     .otherwise({
       redirectTo: '/'
@@ -25,4 +27,7 @@ app.config(function ($routeProvider, $locationProvider){
     //   enabled: true,
     //   requireBase: false
     // });
+  
+  // Stylizes the pagination
+  paginationTemplateProvider.setString(require('html-loader!./shared/parts/data/xxpagination.html'));
 });
