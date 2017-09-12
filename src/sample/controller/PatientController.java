697fc3ba8cfb4b596fb498f2156bebb3444f0e8e
Berthony Delalue <berthony33@gmail.com>
2017-09-12 09:51:31 +0800

    Completed back-end operations for patient

--- src/sample/controller/PatientController.java
+++ src/sample/controller/PatientController.java
@@ -11,65 +11,92 @@ import sample.dto.PatientDto;
 import sample.service.PatientService;
 
 public class PatientController extends Controller {
-    
-    /**
-     * Service object that will be used to call CRUD functions to datastore
-     */
-    PatientService PatientService = new PatientService();
-    
-    /**
-     * For now, used to insert a 'Patient' entity to the datastore
-     */
-    @Override
-    protected Navigation run() throws Exception {
-        System.out.println("PatientController.run start");
-        JSONObject json = new JSONObject();
-        
-        
-        try{
-            /**
-             * Used to retrieve the JSON equivalent data
-             */
-            BufferedReader br = request.getReader();
-            String str = null;
-            StringBuilder sb = new StringBuilder();
-            while ((str = br.readLine()) != null) {
-                sb.append(str);
-            }
-            JSONObject jObj = new JSONObject(sb.toString());
-            
-            /**
-             * Used to store the information from the request and send to the
-             * service class.
-             */
-            PatientDto PatientDto = new PatientDto(jObj.getString("fname"),
-                                                jObj.getString("lname"),
-                                                jObj.getString("address"),
-                                                jObj.getString("email"),
-                                                jObj.getString("number"),
-                                                jObj.getString("birthday"),
-                                                jObj.getString("username"),
-                                                jObj.getString("password")
-                                               );
-            
-            
-           ;
-            
-            if( PatientService.insertPat(PatientDto) == false){
-                json.put("message", "duplicated");
-                  
-            } else {
-                json.put("message", true);
-            }
-            response.setContentType("application/json");
-            response.setCharacterEncoding("utf-8");
-            PrintWriter out = response.getWriter();     
-            //print JSon
-            out.print(json.toString());
-        }catch(Exception e){
-            System.out.println("PatientController.run.exception: "+e.toString());
+
+ /**
+  * Service object that will be used to call CRUD functions to datastore
+  */
+ PatientService patientService = new PatientService();
+
+
+ /**
+  * For now, used to insert a 'patient' entity to the datastore
+  */
+ @Override
+ protected Navigation run() throws Exception {
+  System.out.println("PatientController.run start");
+
+  JSONObject json = new JSONObject();
+  String action = request.getParameter("action");
+ 
+  
+        /** 
+         * Used to retrieve the JSON equivalent data
+         */
+        BufferedReader br = request.getReader();
+        String str = null;
+        StringBuilder sb = new StringBuilder();
+        while ((str = br.readLine()) != null) {
+            sb.append(str);
         }
-        
+        JSONObject jObj = new JSONObject(sb.toString());
+
+        /**
+         * Used to store the information from the request and send to the
+         * service class.
+         */
+        PatientDto patientDto = new PatientDto(jObj.getString("fname"),
+            jObj.getString("lname"),
+            jObj.getString("address"),
+            jObj.getString("email"),
+            jObj.getString("number"),
+            jObj.getString("birthday"),
+            jObj.getString("username"),
+            jObj.getString("password")
+           );
+
+        if (action.equals("registerPatient")) {
+
+
+            if (patientService.insertPat(patientDto) == false) {
+
+                json.put("message", "duplicated");
+
+            } else {
+
+                json.put("message", true);
+
+            }
+
+        } else if (action.equals("updatePatient")) {
+
+
+            if (PatientService.updatePatient(patientDto)) {
+                json.put("message", "The Profil of  " + jObj.getString("lname") + " was updated ");
+            } else {
+                json.put("message", false);
+            }
+
+        } else if (action.equals("deletePatient")) {
+            if (PatientService.deletePatient(patientDto)) {
+
+                json.put("message", "The Profil of " + jObj.getString("lname") + " was deleted ");
+            } else {
+                json.put("message", false);
+            }
+
+        } else if (action.equals("getPatient")) {
+
+            json.put("patients", PatientService.getPatient(Long.parseLong(jObj.getString("id"))));
+        } else if (action.equals("getPatients")) {
+            json.put("patients", PatientService.getPatients());
+        }
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        //print JSon
+        out.print(json.toString());
+
         System.out.println("PatientController.run end");
         //screen redirection.
         return null;
--- src/sample/dao/PatientDao.java
+++ src/sample/dao/PatientDao.java
@@ -1,13 +1,21 @@
 package sample.dao;
 
+import java.util.ArrayList;
+
 import org.slim3.datastore.Datastore;
 
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Transaction;
 
+import sample.meta.PatientModelMeta;
 import sample.model.PatientModel;
+
 
 public class PatientDao{
     
@@ -16,7 +24,7 @@ public class PatientDao{
      * @param inputPat - contains the information for verification
      * @return PatientModel returned by the query
      */
-    public PatientModel getPat(PatientModel inputPat){
+    public static PatientModel getPat(PatientModel inputPat){
         System.out.println("PatientDao.getPat start");
         
         if(Datastore.query("PatientModel")
@@ -34,10 +42,74 @@ public class PatientDao{
     }
     
     /**
+     * Used to get only a single 'Patient' from the datastore with the same 'name'
+     * @param inputPat - contains the information for verification
+     * @return PatientModel returned by the query
+     */
+    public PatientModel getPatientById(Long id){
+        System.out.println("PatientDao.getPat start");
+        PatientModel patient =    PatientModelMeta.get().entityToModel(
+            Datastore.query("PatientModel")
+                     .filter("id", FilterOperator.EQUAL, id)
+                     .asSingleEntity()) ;
+        if(patient != null){
+ 
+            System.out.println("PatientDao.getPat end(success)");
+            return patient;
+        }else{
+            System.out.println("PatientDao.getPat end(failed)");
+            return null;
+        }
+    }
+    
+    public boolean getPatientByEmail(String email){
+        System.out.println("PatientDao.getPat start");
+      
+        if(
+            Datastore.query("PatientModel")
+            .filter("email", FilterOperator.EQUAL, email)
+            .asSingleEntity() != null){
+ 
+            System.out.println("PatientDao.getPat end(success)");
+            return true;
+        }else{
+            System.out.println("PatientDao.getPat end(failed)");
+            return false;
+        }
+    }
+    
+    public  Object getPatients(){
+        com.google.appengine.api.datastore.DatastoreService datastore = DatastoreServiceFactory
+                .getDatastoreService();
+                ArrayList<PatientModel> results =  new ArrayList<PatientModel>();
+
+               Query query = new Query("PatientModel");
+                java.util.List<Entity> entities = datastore.prepare(query).asList(
+                 FetchOptions.Builder.withDefaults());
+
+               for (Entity entity : entities) {
+                 results.add(PatientModelMeta.get().entityToModel(entity));
+               }
+               
+            return results ;
+        
+    }
+    public void updatePatient(PatientModel inputPatient) {
+        System.out.println("PatientDao.updatePatient " + "start");
+        // TODO: Implement this function.
+        Transaction trans = Datastore.beginTransaction();
+        
+        Datastore.put(trans, inputPatient);
+        
+        trans.commit();
+        System.out.println("PatientDao.updatePatient " + "end");
+    }
+    
+    /**
      * Used to insert the 'Patient' to the datastore
      * @param inputPat - the item to be inserted
      */
-    public void insertPat(PatientModel inputPat){
+    public static void insertPat(PatientModel inputPat){
         System.out.println("PatientDao.insertPat start");
         Transaction trans = Datastore.beginTransaction();
         
@@ -53,5 +125,17 @@ public class PatientDao{
         Datastore.put(inputPat);
         trans.commit();
         System.out.println("PatientDao.insertPat end");
+    }
+    public void deletePatient(PatientModel inputPat) {
+        // TODO Auto-generated method stub
+
+            System.out.println("PatientDao.deletePatient " + "start");
+            // TODO: Implement this function.
+            Transaction trans = Datastore.beginTransaction();
+            Datastore.put(inputPat);
+            trans.commit();
+            System.out.println("PatientDao.deletePatient " + "end");
+        
+        
     }
 }
