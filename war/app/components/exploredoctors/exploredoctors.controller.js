8b5307e1847dfd888e83715984c8d9c98ab2bff1
redsaph <redsaph@ymail.com>
2017-09-14 15:09:28 +0800

    Fixed DoctorController in JAVA and doctorFactory in angular.

--- war/app/components/exploredoctors/exploredoctors.controller.js
+++ war/app/components/exploredoctors/exploredoctors.controller.js
@@ -1,39 +1,20 @@
 angular.module('hplus.modules.exploredoctors')
 
   .controller('ExploreDoctorsController',
-    function($scope, globalFactory, modalFactory, $http){
+    function($scope, globalFactory, modalFactory, doctorFactory){
 
       $scope.go = function(path){
         globalFactory.go(path);
       };
-
-      // $scope.doctorList = [
-      //   {primaryText: "Daffy Duck", secondaryText: "Optometry"},
-      //   {primaryText: "Tom Cat", secondaryText: "Dentistry"},
-      //   {primaryText: "Jerry Mouse", secondaryText: "Cardiology"},
-      //   {primaryText: "Sylvester Cat", secondaryText: "Oncology"},
-      //   {primaryText: "Nemo Fish", secondaryText: "EENT"},
-      //   {primaryText: "Mickey Mouse", secondaryText: "General Surgery"},
-      //   {primaryText: "Scooby Doo", secondaryText: "Anesthesiology"},
-      //   {primaryText: "Courage Dog", secondaryText: "Neurology"},
-      //   {primaryText: "Taron Egerton", secondaryText: "Alcohology"},
-      //   {primaryText: "Tasmanian Devil", secondaryText: "Nephrology"},
-      //   {primaryText: "Perry Platypus", secondaryText: "Mixology"}
-      // ];
+      
+      $scope.doctorList = [];
 
       var populate = function(){
-        $http({
-          method: "POST",
-          url: "/Doctor",
-          data: {
-            action: "getDoctors"
-          }
-        }).then(function(response){
-          console.log(response);
+        doctorFactory.getListOfDoctors().then(function(response){
+          $scope.doctorList = response.data.doctors;
         }, function(response){
-          console.log("I got none.");
-          console.log(response);
-        })
+          console.log(response.statusText);
+        });
       }
 
       populate();
