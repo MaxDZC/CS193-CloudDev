6934571148ca5eb4a1572bfb82e90d8aaf053cee
redsaph <redsaph@ymail.com>
2017-09-14 15:17:48 +0800

    Changed modalCtrl as modalController?

--- src/sample/controller/DoctorController.java
+++ src/sample/controller/DoctorController.java
@@ -27,8 +27,10 @@ public class DoctorController extends Controller {
 
   JSONObject json = new JSONObject();
   String action = request.getParameter("action");
- 
+  String method = request.getMethod();
+  JSONObject jObj = null;
   
+  if(method == "POST"){
         /** 
          * Used to retrieve the JSON equivalent data
          */
@@ -38,7 +40,7 @@ public class DoctorController extends Controller {
         while ((str = br.readLine()) != null) {
             sb.append(str);
         }
-        JSONObject jObj = new JSONObject(sb.toString());
+        jObj = new JSONObject(sb.toString());
 
         /**
          * Used to store the information from the request and send to the
@@ -56,6 +58,7 @@ public class DoctorController extends Controller {
             Long.parseLong(jObj.getString("id"))
 
         );
+        
         if (action.equals("registerDoctor")) {
 
 
@@ -86,12 +89,14 @@ public class DoctorController extends Controller {
                 json.put("message", false);
             }
 
-        } else if (action.equals("getDoctor")) {
-
-            json.put("doctors", DoctorService.getDoctor(Long.parseLong(jObj.getString("id"))));
-        } else if (action.equals("getDoctors")) {
-            json.put("doctors", DoctorService.getDoctors());
         }
+  } else if (method == "GET") {
+      if(this.request.getParameter("id") != null){
+        json.put("doctors", DoctorService.getDoctor(Long.parseLong(this.request.getParameter("id"))));
+     } else {
+        json.put("doctors", DoctorService.getDoctors());
+     }
+  }
 
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
