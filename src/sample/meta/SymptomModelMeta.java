a58213d5d9bdc95af6f9a4bbd827c5553f72f947
Addrian <addcarl@example.com>
2017-09-20 16:00:21 +0800

    meta

--- src/sample/meta/SymptomModelMeta.java
+++ src/sample/meta/SymptomModelMeta.java
@@ -1,6 +1,6 @@
 package sample.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-20 13:32:29")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-20 15:59:21")
 /** */
 public final class SymptomModelMeta extends org.slim3.datastore.ModelMeta<sample.model.SymptomModel> {
 
