af6bb2fed1050d49264279278de2fb44e4240398
gerahme <gerahmehambunan@gmail.com>
2017-09-21 19:21:51 +0800

    explore patient updated

--- src/sample/meta/DoctorModelMeta.java
+++ src/sample/meta/DoctorModelMeta.java
@@ -1,6 +1,6 @@
 package sample.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-20 14:05:39")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-20 16:41:24")
 /** */
 public final class DoctorModelMeta extends org.slim3.datastore.ModelMeta<sample.model.DoctorModel> {
 
