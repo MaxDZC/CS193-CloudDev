a326ab533e3a4cdc374fa84aa015db335ebaeec6
Patricia Awayan <pawayan1@yahoo.com>
2017-09-25 02:11:54 +0800

    metaa

--- src/sample/meta/DiseaseModelMeta.java
+++ src/sample/meta/DiseaseModelMeta.java
@@ -1,6 +1,6 @@
 package sample.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-24 18:17:47")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-25 02:09:04")
 /** */
 public final class DiseaseModelMeta extends org.slim3.datastore.ModelMeta<sample.model.DiseaseModel> {
 
--- src/sample/meta/PatientModelMeta.java
+++ src/sample/meta/PatientModelMeta.java
@@ -1,6 +1,6 @@
 package sample.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-24 18:17:49")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2017-09-25 02:09:05")
 /** */
 public final class PatientModelMeta extends org.slim3.datastore.ModelMeta<sample.model.PatientModel> {
 
