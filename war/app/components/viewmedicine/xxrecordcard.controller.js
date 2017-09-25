eab0aa246b43056040ebfef4b1705b699a6671c2
Max <maxtimedout@gmail.com>
2017-09-26 04:45:55 +0800

    Disease 90% done, displaying nlng

--- war/app/components/viewmedicine/xxrecordcard.controller.js
+++ war/app/components/viewmedicine/xxrecordcard.controller.js
@@ -1,8 +1,10 @@
 angular.module('hplus.modules.viewmedicine')
 
   .controller('RecordCardController',
-    function($scope, $location, globalFactory, medicalRecordFactory){
+    function($scope, $location, globalFactory, doctorFactory, medicalRecordFactory){
       
+      $scope.user = doctorFactory.getUser();
+
       $scope.go = function(path, medicalRecord){
         medicalRecordFactory.saveMedicalRecord(medicalRecord);
         globalFactory.go(path);
